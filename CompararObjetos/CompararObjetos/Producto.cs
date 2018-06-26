89ecf0f61a37f0e8416a2b469d2a6f12dab2bf47
ProgrammeRoy <38889074+ProgrammeRoy@users.noreply.github.com>
2018-06-26 16:36:39 -0500

    Comparando Objetos

---
+++ CompararObjetos/CompararObjetos/Producto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompararObjetos
+{
+    // Luego de los dos puntos crea un metodo para comparar <>
+    class Producto: IComparable<Producto>
+    {
+        public string Codigo { get; set; }
+        public decimal Precio { get; set; }
+
+        //Este es el método creado para comparar
+        public int CompareTo(Producto otro)
+        {
+            // >0 mayor
+            if (this.Precio > otro.Precio) return 1;
+            // == 0 igual
+            if (this.Precio == otro.Precio) return 0;
+            // <0 menor
+            return -1;
+        }
+    }
+}
---
+++ CompararObjetos/CompararObjetos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompararObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto P1 = new Producto { Codigo = "A1", Precio = 123 };
+            Producto P2 = new Producto { Codigo = "A2", Precio = 34.5m };
+            Producto P3 = new Producto { Codigo = "A3", Precio = 99.9m };
+
+            Producto[] productos = new Producto[] { P1, P2, P3 };
+            Array.Sort(productos);
+
+            foreach (var prod in productos)
+            {
+                Console.WriteLine("Codigo: {0}, Precio {1}", prod.Codigo, prod.Precio);
+            }
+        }
+    }
+}

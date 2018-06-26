d86c351c23c890800e25a292998936cea0175304
Samu Marw <sam_chacla@hotmail.com>
2018-06-26 11:32:01 -0500

    Comentarios agregados de como se leería algunos miembros de las clases

--- HerenciaEstudiante/HerenciaEjercicio2/Estudiante.cs
+++ HerenciaEstudiante/HerenciaEjercicio2/Estudiante.cs
@@ -6,26 +6,38 @@ using System.Threading.Tasks;
 
 namespace HerenciaEjercicio2
 {
-    abstract class Estudiante
+  abstract class Estudiante
+  {
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+
+    // Propiedad abstract debe definirse en las clases hereadads
+    public abstract double PensionBeca { get; }
+
+    //primer constructor sin parametros
+    public Estudiante() { }
+
+    //segundo constructor con 2 parametros
+    public Estudiante(string nombre, int edad)
     {
-        public string Nombre { get; set; }
-        public int Edad { get; set; }
+      // Inicializando 2 propiedades según los 2 parámetros de ingreso
+      Nombre = nombre;
+      Edad = edad;
 
-        public abstract double PensionBeca { get;}
-
-        public Estudiante() { }
-
-        public Estudiante(string nombre, int edad)
-        {
-            Nombre = nombre;
-            Edad = edad;
-        }
-
-        public void Saludar()
-        {
-            Console.WriteLine("Hola, mi nombre es {0} y tengo {1} años",Nombre,Edad);
-        }
-
-        public abstract void becado();
     }
+
+    // implemnetado en clase abstracta, en las clases heredadas NO SE PUEDE MODIFICAR
+    public void Saludar()
+    {
+      Console.WriteLine("Hola, mi nombre es {0} y tengo {1} años", Nombre, Edad);
+    }
+    //Metodo implemnetado en clase abstracta, en las clases heredadas SI SE PUEDE MODIFICAR
+    public virtual void MetodoCambiable()
+    {
+      Console.WriteLine("Este metodo está definido pero puede cambiarse");
+    }
+
+    // Metodo que debe implementarse obligatoriamente en los hijos
+    public abstract void becado();
+  }
 }
--- HerenciaEstudiante/HerenciaEjercicio2/EstudianteBecaCompleta.cs
+++ HerenciaEstudiante/HerenciaEjercicio2/EstudianteBecaCompleta.cs
@@ -6,32 +6,40 @@ using System.Threading.Tasks;
 
 namespace HerenciaEjercicio2
 {
-    class EstudianteBecaCompleta: Estudiante
+  class EstudianteBecaCompleta : Estudiante
+  {
+    public double _Beca { get; set; }
+
+    //Sobreescribiendo necesariamente (porque era abstract) el método GET de la propiedad "PensionBeca"
+    public override double PensionBeca
     {
-        public double  _Beca { get; set; }
-
-        public override double PensionBeca
-        {
-            get
-            {
-                return _Beca*0;
-            }
-        }
-
-        public EstudianteBecaCompleta(string nombre, int edad, double _beca): base(nombre,edad)
-        {
-            _Beca = _beca;
-        }
-
-        public override void becado()
-        {
-            Console.WriteLine("La pensión regular para mi carrera es {0} soles", _Beca);
-            Console.WriteLine("Mi pensión por beca es {0} soles", PensionBeca);
-        }
-
-        public override string ToString()
-        {
-            return string.Format("{0}, {1} años, 100 % de beca", Nombre,Edad); 
-        }
+      get
+      {
+        return _Beca * 0;
+      }
     }
+
+    //Recibe 3 parámetros, pero en esta clase solo inicializa uno, los otros 2 son inicianlizados en la clase "base" (osea Estudiante)
+    public EstudianteBecaCompleta(string nombre, int edad, double _beca) : base(nombre, edad)
+    {
+      _Beca = _beca;
+    }
+
+    public override void becado()
+    {
+      Console.WriteLine("La pensión regular para mi carrera es {0} soles", _Beca);
+      Console.WriteLine("Mi pensión por beca es {0} soles", PensionBeca);
+    }
+
+    public override void MetodoCambiable()
+    {
+      Console.WriteLine("Modifique el metodo");
+    }
+
+    public override string ToString()
+    {
+      return string.Format("{0}, {1} años, 100 % de beca", Nombre, Edad);
+    }
+
+  }
 }
